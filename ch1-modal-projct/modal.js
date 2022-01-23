c0737806eebc23bb7d64e5b123a260601d223752
3기-1반 애범 <gogot1103@naver.com>
2022-01-23 18:47:12 +0900

    남기범/weel1/modal-project 수정

--- ch1-modal-projct/modal.js
+++ ch1-modal-projct/modal.js
@@ -1,16 +1,43 @@
+// animal
 const chickenModalBtn = document.querySelector(".chicken");
-const BeerModalBtn = document.querySelector(".beer");
+const beerModalBtn = document.querySelector(".beer");
+const jsModalBtn = document.querySelector(".js");
+const dogModalBtn = document.querySelector(".dog");
+
+// handle modal
 const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
+const animalName = document.getElementById("name");
 
-chickenModalBtn.addEventListener("click", () => {
-  modal.classList.add("open-chicken");
+const openModal = (e) => {
+  const id = e.target.id;
+  const animal = e.target.name;
+  animalName.textContent = animal;
+  modal.classList.add(`open-${id}`);
+};
+
+chickenModalBtn.addEventListener("click", (e) => {
+  openModal(e);
 });
 
-BeerModalBtn.addEventListener("click", () => {
-  modal.classList.add("open-beer");
+beerModalBtn.addEventListener("click", (e) => {
+  openModal(e);
+});
+
+jsModalBtn.addEventListener("click", (e) => {
+  openModal(e);
+});
+
+dogModalBtn.addEventListener("click", (e) => {
+  openModal(e);
 });
 
 closeBtn.addEventListener("click", () => {
-  modal.classList.remove("open-chicken", "open-beer");
+  modal.classList.remove("open-chicken", "open-beer", "open-js", "open-dog");
+  animalName.textContent = "";
+});
+
+modal.addEventListener("click", () => {
+  modal.classList.remove("open-chicken", "open-beer", "open-js", "open-dog");
+  animalName.textContent = "";
 });
