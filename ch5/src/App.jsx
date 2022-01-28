1b18bde8fea8f9e6119ac6303f5d6a7b25e44434
beom <gogot1103@naver.com>
2022-01-28 15:20:27 +0900

    Thee.js 실습 세팅

---
+++ ch5/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import { Canvas } from "react-three-fiber";
+// import { OrbitControls, Stars } from "drei";
+// import { Physics, usePlane, useBox } from "use-cannon";
+import "./styles.css";
+
+function App() {}
+
+export default App;
